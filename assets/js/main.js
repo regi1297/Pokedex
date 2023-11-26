216304c1b4a36760abfc90f79b664bb7ba3a4fa0
regi1297 <reginald_miranda@hotmail.com>
2023-11-26 20:51:42 -0300

    Manipulando resultado de requisição JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('Sucesso!')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+fetch(url)
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (erro) {
+  console.error(erro);
+})
+.finally(function () {
+  console.log('Requisição concluída!');
+})
+
