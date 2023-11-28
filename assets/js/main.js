0e72b5b97932388f67b9bedbc76663ce74e24b81
regi1297 <reginald_miranda@hotmail.com>
2023-11-27 21:05:07 -0300

    manipulando response

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 fetch(url)
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (erro) {
-  console.error(erro);
-})
-.finally(function () {
-  console.log('Requisição concluída!');
-})
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error(error))
 
