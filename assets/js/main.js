ee7e207cd397dbf928852f7b94af00d6afd99ac0
regi1297 <reginald_miranda@hotmail.com>
2023-11-28 21:53:05 -0300

    Convertendo em função map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
@@ -23,17 +19,8 @@ function convertPokemonToLi(pokemon) {
 }
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
+pokeApi.getPokemons().then((pokemons = []) => {
+  
+  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("")
 
-  for (let i = 0; i < pokemons.length; i++) {
-    const pokemon = pokemons[i];
-    pokemonList.innerHTML += convertPokemonToLi(pokemon)
-    
-  }
 })
-
-.catch((error) => console.error(error))
-
